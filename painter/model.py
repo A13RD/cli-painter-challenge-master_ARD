3dc07b9f3a9b7acd9510d72e38dd4b2746c139c5
A13RD <alejandroruizdiaz0221@gmail.com>
2024-08-05 18:37:41 -0500

    Class Circle

--- painter/model.py
+++ painter/model.py
@@ -3,3 +3,20 @@ class Point:
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
+
+class Circle:
+    def __init__(self, center: Point, radius: float):
+        self.center: center
+        self.radius: radius
+
+    def area(self) -> float:
+        return 3.1416 * (self.radius ** 2)
+
+    def draw(self):
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
+
+    def __str__(self) -> str:
+        return f"Circle with center at ({self.center.x}, {self.center.y}) and radius {self.radius}"
