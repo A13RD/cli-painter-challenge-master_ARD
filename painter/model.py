8c8a42e636d669815ed23fa1e6af516da1664e57
A13RD <alejandroruizdiaz0221@gmail.com>
2024-08-05 20:56:12 -0500

    Class Painter

--- painter/model.py
+++ painter/model.py
@@ -1,4 +1,33 @@
 import matplotlib.pyplot as plt
+import pickle
+class Painter:
+    FILE = ".painter"
+
+    def __init__(self) -> None:
+        self.shapes: list = []
+        self._load()
+
+    def _load(self) -> None:
+        try:
+            with open(Painter.FILE, "rb") as f:
+                self.shapes = pickle.load(f)
+        except (EOFError, FileNotFoundError):
+            self.shapes = []
+
+    def _save(self) -> None:
+        with open(Painter.FILE, "wb") as f:
+            pickle.dump(self.shapes, f)
+
+    def add_shape(self, shape) -> None:
+        self.shapes.append(shape)
+        self._save()
+
+    def total_area(self) -> float:
+        return sum(shape.area() for shape in self.shapes)
+
+    def clear(self) -> None:
+        self.shapes = []
+        self._save()
 class Point:
     def __init__(self, x: float, y: float):
         self.x = x
